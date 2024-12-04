70af9ad3ab6cc551acdf3b1959efddde0f2074fb
1nonlyvinh <vincenth2898@tuckahoeschools.org>
2024-12-04 13:32:24 +0000

    yipee

--- Loop Examples/doWhileExample.js
+++ Loop Examples/doWhileExample.js
@@ -1,1 +1,26 @@
 
+let outputDiv = document.getElementById('output');
+let passwordInput = document.getElementById('passwordInput');
+let submitButton = document.getElementById('submitButton'); 
+
+const correctPassword = 'password';
+outputDiv.innerHTML += "<h2>Using a password Prompt</h2>";
+
+submitButton.addEventListener('click', () => {
+    let password; 
+    let attempt = 0;
+
+    do{
+        password = passwordInput.value 
+
+        if(password === correctPassword){
+            outputDiv.innerHTML += "access granted!<br>";
+            passwordInput.disabled = true; 
+            submitButton.disabled = true; 
+            break; 
+        }else {
+            outputDiv.innerHTML += `Incorrect password. Attempt ${++attempt}. try again <br>`;
+            passwordInput.value = ""; 
+        }
+    } while (password !== correctPassword && !passwordInput.disabled);
+});
---
+++ Loop Examples/whileExample.js
@@ -0,0 +1,9 @@
+let diceRoll; 
+let rolls = 0;
+
+while(diceRoll !== 6){
+    diceRoll = Math.floor( Math.random() * 6) + 1;
+    rolls++; 
+    console.log(`you rolled: ${diceRoll}`);
+}
+console.log(`it took ${rolls} rolls to get a 6!`);
